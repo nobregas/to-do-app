9b95c6b253449ab731148aca827f324b4f8a10ac
nobrega <gabrielnobregasantos2@gmail.com>
2025-07-08 18:51:48 -0300

    fix: fixed return types

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Http\Resources\TaskResource;
+use App\Http\Resources\LoginResource;
+use App\Http\Resources\RegisterResource;
 use App\Services\Interface\AuthServiceInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Request;
@@ -16,12 +17,12 @@ class AuthController extends Controller
     {
     }
 
-    public function register(RegisterRequest $request):  TaskResource
+    public function register(RegisterRequest $request):  RegisterResource
     {
         return $this->authService->register($request);
     }
 
-    public function login(LoginRequest $request):  TaskResource
+    public function login(LoginRequest $request):  LoginResource
     {
         return $this->authService->login($request);
     }
