8bafdac27f5d59547b245d058919ff14aa58fc46
nobrega <gabrielnobregasantos2@gmail.com>
2025-07-08 18:59:03 -0300

    fix: return types

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -5,7 +5,10 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Task\StoreTaskRequest;
 use App\Http\Requests\Task\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
 use App\Services\Interface\TaskServiceInterface;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class TaskController extends Controller
 {
@@ -17,12 +20,12 @@ class TaskController extends Controller
         $this->taskService = $taskService;
     }
 
-    public function index()
+    public function index(): AnonymousResourceCollection
     {
         return $this->taskService->findAll();
     }
 
-    public function store(StoreTaskRequest $request)
+    public function store(StoreTaskRequest $request): JsonResponse
     {
         $task = $this->taskService->create($request);
 
@@ -31,17 +34,17 @@ class TaskController extends Controller
             ->setStatusCode(201);
     }
 
-    public function show($id)
+    public function show(int $id): TaskResource
     {
         return $this->taskService->find($id);
     }
 
-    public function update(UpdateTaskRequest $request, $id)
+    public function update(UpdateTaskRequest $request, int $id): TaskResource
     {
         return $this->taskService->update($id, $request);
     }
 
-    public function destroy($id)
+    public function destroy(int $id): JsonResponse
     {
         $this->taskService->delete($id);
 
