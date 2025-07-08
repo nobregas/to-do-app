1d7cfd78cc6f76a113389a265df8677d17583a55
nobrega <gabrielnobregasantos2@gmail.com>
2025-07-08 17:40:13 -0300

    refactor: add readonly to authservice field

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Request;
 
 class AuthController extends Controller
 {
-    public function __construct(private AuthServiceInterface $authService)
+    public function __construct(private readonly AuthServiceInterface $authService)
     {
     }
 
